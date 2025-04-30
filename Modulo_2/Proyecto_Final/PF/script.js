4321aa54e66ec41e4893da42460d560e411da0fa
unknown <cami.rivas.dlb@gmail.com>
2025-04-30 18:38:37 -0400

    se inicia forma PF

--- Modulo_2/Proyecto_Final/PF/script.js
+++ Modulo_2/Proyecto_Final/PF/script.js
@@ -1,3 +1,12 @@
-function saludar(nombre) {
-    return `Hola, ${nombre}`;
+// === Base de datos: array de encuestas ===
+const encuestas = [];
+
+function crearPregunta(texto, opciones) {
+    if (opciones.length < 2) {
+      console.error("Una pregunta necesita al menos 2 opciones.");
+      return null;
+    }
+    const votos = opciones.map(() => 0); // Inicializa votos con ceros
+    return [texto, opciones, votos]; // Array en lugar de objeto ????? esta bien que devuelva eso???
   }
+

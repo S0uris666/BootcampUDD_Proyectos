df793405091a84372f1b210d7cba6690688356f2
unknown <cami.rivas.dlb@gmail.com>
2025-05-07 15:18:13 -0400

    Actualziacion reto 8

--- Modulo_2/Retos/Reto_8/Reto_8.js
+++ Modulo_2/Retos/Reto_8/Reto_8.js
@@ -21,4 +21,43 @@ const ventas = [
       ],
     },
   ];
+    //Debes calcular el subtotal de cada venta, el total de las ventas, 
+  // extraer todos los productos vendidos y calcular la cantidad de productos vendidos 
+  // por categoría 
+  // (smartphones, laptops y cámaras).
+  function procesarVentas(ventas) {
+    let totalVentas = 0;
+    let productosVendidos = [];
+    let cantidadPorCategoria = {
+      smartphone: 0,
+      laptop: 0,
+      cámara: 0
+    };
   
+    ventas.forEach(venta => {
+      let subtotal = 0;
+  
+      venta.productos.forEach(producto => {
+        const subtotalProducto = producto.precio * producto.cantidad;
+        subtotal += subtotalProducto;
+        totalVentas += subtotalProducto;
+  
+        productosVendidos.push({
+          cliente: venta.cliente,
+          ...producto
+        });
+  
+        if (cantidadPorCategoria.hasOwnProperty(producto.nombre)) {
+          cantidadPorCategoria[producto.nombre] += producto.cantidad;
+        }
+      });
+  
+      console.log(`Subtotal de la venta de ${venta.cliente}: $${subtotal}`);
+    });
+  
+    console.log('Total de todas las ventas:', totalVentas);
+    console.log('Productos vendidos:', productosVendidos);
+    console.log('Cantidad de productos vendidos por categoría:', cantidadPorCategoria);
+  }
+
+  procesarVentas(ventas);
